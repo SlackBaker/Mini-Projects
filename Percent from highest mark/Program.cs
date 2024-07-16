9c117e8a141c5b25c25dd78e02e5a50be1ffc4da
SlackBaker <ostapanonim228@gmail.com>
2024-07-16 17:45:29 +0300

    Add files via upload

---
+++ Percent from highest mark/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MarkPercent
+{
+    class Program
+    {
+        string mark;
+        int mark2;
+        static int maxmark = 12; //12 because this is maximal mark in Ukraine.
+
+        void CalculatePercent()
+        {
+            int calculate = this.mark2 * 100 / Program.maxmark;
+            Console.WriteLine(calculate + "%");
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            Console.WriteLine("Write mark which you have got to convert it into the percents from highest mark");
+            pr.mark = Console.ReadLine();
+            pr.mark2 = Convert.ToInt32(pr.mark);
+            pr.CalculatePercent();
+        }
+    }
+
+}
